018f63525a4b450119fe02bd08fa2a01c48ecec8
David Ferrante <dmf07@hotmail.com>
2020-03-19 20:04:47 -0400

    barcode spider

--- app/main.py
+++ app/main.py
@@ -1,48 +1,18 @@
 from flask import Flask, jsonify, abort
 from flask_cors import CORS, cross_origin
+import requests
 
 app = Flask(__name__)
 CORS(app)
-
-upcs = [
-    {
-        'upc': '0012345678905',
-        'title': u'Sample UPC',
-        'description': u'This is a sample UPC',
-        'imageUrl': u'https://www.upccode.net/sample_codes/UPC%20Code%20sample.jpg',
-        'stores': [
-            {
-                "name": u'Walmart',
-                "price": 100.00
-            },
-            {
-                "name": u'Amazon',
-                "price": 95.00
-            },
-            {
-                "name": u'Target',
-                "price": 100.00
-            }
-        ]
-    }
-]
-
-
-@app.route('/upc', methods=['GET'])
-@cross_origin()
-def get_upcs():
-    return jsonify(upcs)
 
 
 @app.route('/upc/<string:upc>', methods=['GET'])
 @cross_origin()
 def get_upc(upc):
-    for x in upcs:
-        if x['upc'] == upc:
-            return jsonify(x)
-            break
-        else:
-            abort(404)
+    r = requests.get(
+        f'https://api.barcodespider.com/v1/lookup?token=df909e5de789bb50b764&upc={upc}')
+    return r.text
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=80)
